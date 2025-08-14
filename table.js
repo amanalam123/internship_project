9278a4e13a8e455f8eec02a0cef10ba2aa9195dc
Aman Alam <amanalamdumri123@gmail.com>
2025-08-14 17:11:41 +0530

    Initial commit

---
+++ table.js
@@ -0,0 +1,35 @@
+
+// for(n=1; n<=10; n++){
+//     document.write("10*" + n + "="+(10*n)+"<br>");
+// }
+
+// let i = 5;
+// let n = 1;
+
+// while(n <=10){
+//     document.write(i+ "*"+n+ "=" +i*n+"<br>");
+//     n++;
+// }
+
+// let i = 0;
+// let n = 6;
+
+// while(i<n){
+//     let j = 0;
+    
+//     while(j<i){
+//         document.write("* ");
+//         j++;
+//     }
+//     document.write("*"+"<br>");
+//     i++;
+// }
+
+// let n = 1;
+// let i = "* ";
+// while(n<=6){
+//     console.log(i.repeat(n))
+//     document.write(i.repeat(n)+"<br>");
+//     n++;
+// }
+
